d303166046d43a23e60dd96b4148ab9e25126357
Pamela M <pamela-mckee@utulsa.edu>
2017-04-25 18:26:04 -0500

    Better way of serving profile data to page

--- src/pages/myProfile/myProfile.ts
+++ src/pages/myProfile/myProfile.ts
@@ -6,9 +6,39 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'myProfile.html'
 })
 export class myProfile {
+  name = "Pamela";
+
+  age = 20;
+
+  blurb = "Down with that sort of thing";
+
+  likes = [
+      'first like',
+      'second like',
+      'third like'
+      ];
+
+  dislikes = [
+    'first dislike',
+   'second dislike',
+    'third dislike'
+    ];
+
+  hobbies = [
+    'first hobby',
+    'second hobby',
+    'third hobby'
+  ];
+
+  places = [
+    'first place',
+    'second place',
+    'third place'
+  ];  
 
   constructor(public navCtrl: NavController) {
 
   }
+
 
 }
